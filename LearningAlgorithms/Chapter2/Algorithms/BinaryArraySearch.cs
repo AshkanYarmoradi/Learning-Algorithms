a0ff985f43fdda3b48125a34ac0ab05b301b0a8b
Ashkan Yarmoradi <ayarmoradi@gmail.com>
2021-12-25 14:50:53 +0300

    ✨Added Binary Search Chapter Two

---
+++ LearningAlgorithms/Chapter2/Algorithms/BinaryArraySearch.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LearningAlgorithms.Abstracts;
+
+namespace LearningAlgorithms.Chapter2.Algorithms
+{
+    public class BinaryArraySearchRequest : RequestAbstract<bool>
+    {
+        public BinaryArraySearchRequest(int[] sortedArray, int target)
+        {
+            SortedArray = sortedArray;
+            Target = target;
+        }
+
+        public int[] SortedArray { get; set; }
+
+        public int Target { get; set; }
+    }
+
+    public class BinaryArraySearchHandler : RequestHandlerAbstract<BinaryArraySearchRequest, bool>
+    {
+        public override Task<bool> Handle(BinaryArraySearchRequest request, CancellationToken cancellationToken)
+        {
+            var lo = 0;
+            var hi = request.SortedArray.Length - 1;
+
+            while (lo <= hi)
+            {
+                var mid = (hi + lo) / 2;
+
+                if (request.SortedArray[mid] >= request.Target)
+                {
+                    hi = mid - 1;
+                }
+                else if(request.SortedArray[mid] <= request.Target)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    return Task.FromResult(true);
+                }
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}
---
+++ LearningAlgorithms/Chapter2/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningAlgorithms.Chapter2.Algorithms;
+using LearningAlgorithms.Generators;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace LearningAlgorithms.Chapter2
+{
+    public class TestChapter2 : UnitTestBase
+    {
+        private readonly IArrayGenerator _arrayGenerator;
+        
+        public TestChapter2()
+        {
+            _arrayGenerator = _serviceProvider.GetRequiredService<IArrayGenerator>();
+        }
+
+        [Fact]
+        public async Task BinarySearch()
+        {
+            var array = _arrayGenerator.Generate<int>(100000).OrderBy(x=>x).ToArray();
+            var target = array.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+            var binaryArraySearch = await _mediator.Send(new BinaryArraySearchRequest(array, target));
+            var improvedBinaryArraySearch = await _mediator.Send(new ImprovedBinaryArraySearchRequest(array, target));
+        }
+    }
+}
